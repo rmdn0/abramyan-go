4aa05015f3fc961b31adfa7ea3b0bc4a37c7bc67
Siyovush <paraparadox@protonmail.com>
2021-12-01 00:12:38 +0500

    Begin 16-20

---
+++ begin/begin17.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+import "math"
+
+func main() {
+	var a, b, c float64
+	fmt.Print("a = ")
+	fmt.Scan(&a)
+	fmt.Print("b = ")
+	fmt.Scan(&b)
+	fmt.Print("c = ")
+	fmt.Scan(&c)
+
+	var ac = math.Abs(c - a)
+	var bc = math.Abs(c - b)
+
+	fmt.Printf("\nAC = %.2f\n", ac)
+	fmt.Printf("BC = %.2f\n", bc)
+	fmt.Printf("AC + BC = %.2f\n", ac + bc)
+}
---
+++ begin/begin18.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a, b, c float64
+	fmt.Print("a = ")
+	fmt.Scan(&a)
+	fmt.Print("b = ")
+	fmt.Scan(&b)
+	fmt.Print("c = ")
+	fmt.Scan(&c)
+
+	ac := math.Abs(c - a)
+	bc := math.Abs(c - b)
+
+	fmt.Printf("\nAC * BC = %.2f\n", ac * bc)
+}
