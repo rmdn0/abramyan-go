019c8885a7b66398ad1c98c06074d7b440957ce7
Siyovush <paraparadox@protonmail.com>
2021-11-30 22:07:16 +0500

    Begin 6

---
+++ begin6.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c float32
+	fmt.Println("Введите ребра прямоугольного параллелепипеда:")
+	fmt.Print("a = ")
+	fmt.Scan(&a)
+	fmt.Print("b = ")
+	fmt.Scan(&b)
+	fmt.Print("c = ")
+	fmt.Scan(&c)
+
+	v := a * b * c
+	s := 2 * (a * b + b * c + c * a)
+	fmt.Println("\nОбъем и площадь поверхности:")
+	fmt.Println("V =", fmt.Sprintf("%.3f", v), "\nS =", fmt.Sprintf("%.3f", s))
+}
