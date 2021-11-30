1d36f5d3ca7c9485d9cda522970fab9f20b49c80
Siyovush <paraparadox@protonmail.com>
2021-11-30 23:21:45 +0500

    Begin 10

---
+++ begin/begin10.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a, b float64
+	fmt.Print("A = ")
+	fmt.Scan(&a)
+	fmt.Print("B = ")
+	fmt.Scan(&b)
+	fmt.Printf("\nA^2 + B^2 = %.2f\n", math.Pow(a, 2) + math.Pow(b, 2))
+	fmt.Printf("A^2 - B^2 = %.2f\n", math.Pow(a, 2) - math.Pow(b, 2))
+	fmt.Printf("A^2 * B^2 = %.2f\n", math.Pow(a, 2) * math.Pow(b, 2))
+	fmt.Printf("A^2 / B^2 = %.2f\n", math.Pow(a, 2) / math.Pow(b, 2))
+}
