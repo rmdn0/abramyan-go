e8a43450cd7659f40eb337a0989ceef514d7e512
Siyovush <paraparadox@protonmail.com>
2021-11-30 20:47:37 +0500

    Begin 2

--- begin2.go
+++ begin2.go
@@ -6,5 +6,5 @@ func main() {
 	var a float32
 	fmt.Print("Введите сторону квадрата:\na = ")
 	fmt.Scan(&a)
-	fmt.Println("Периметр квадрата со стороной a:\nP =", fmt.Sprintf("%.2f", 4 * a))
+	fmt.Println("Площадь квадрата со стороной a:\nS =", fmt.Sprintf("%.2f", a * a))
 }
