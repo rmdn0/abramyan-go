f6bae2592733df54028daa4f41125c8f6abc7afb
Siyovush <paraparadox@protonmail.com>
2021-11-30 23:34:40 +0500

    Begin 12

---
+++ begin/begin12.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+import "math"
+
+func main() {
+	var a, b float64
+	fmt.Print("a = ")
+	fmt.Scan(&a)
+	fmt.Print("b = ")
+	fmt.Scan(&b)
+	c := math.Sqrt(a * a + b * b)
+	p := a + b + c
+	fmt.Printf("\nc = %.2f\n", c)
+	fmt.Printf("p = %.2f\n", p)
+}
