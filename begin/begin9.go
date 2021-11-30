a7346a007bf67478fcb1720f6e8228ddedddf59b
Siyovush <paraparadox@protonmail.com>
2021-11-30 23:15:42 +0500

    Begin 9

---
+++ begin/begin9.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a, b float32
+	fmt.Println("Введите два неотрицательных числа:")
+	fmt.Print("a = ")
+	fmt.Scan(&a)
+	fmt.Print("b = ")
+	fmt.Scan(&b)
+	fmt.Println("Среднее геометрическое введенных чисел:")
+	fmt.Printf("%.2f\n", math.Sqrt(float64(a * b)))
+}
