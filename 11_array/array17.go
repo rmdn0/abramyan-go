f7556e3e35ef8129bea518a79e81c38285ca2f43
Siyovush <paraparadox@protonmail.com>
2022-02-15 20:34:21 +0500

    Array 11-20

---
+++ 11_array/array17.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Print("N = ")
+	fmt.Scan(&n)
+
+	ar := make([]float64, n, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&ar[i])
+	}
+
+	step := n + 1
+	trigger := -1
+
+	for i := 0; step > 1; i += step * trigger {
+		trigger *= -1
+		step -= 2
+		fmt.Print(ar[i], " ")
+		if step != 0 {
+			fmt.Print(ar[i+(1*trigger)], " ")
+		}
+	}
+	fmt.Println()
+}
---
+++ 11_array/array20.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("N = ")
+	fmt.Scan(&n)
+
+	ar := make([]float64, n, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&ar[i])
+	}
+
+	var k, l int
+	fmt.Print("K = ")
+	fmt.Scan(&k)
+	fmt.Print("L = ")
+	fmt.Scan(&l)
+
+	var sum float64
+
+	for i := k - 1; i < l; i++ {
+		sum += ar[i]
+	}
+
+	fmt.Printf("\n%.2f\n", sum)
+}
