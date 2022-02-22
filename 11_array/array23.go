173c1e626ec283a11f00787fd4636b4c1288d025
Siyovush <paraparadox@protonmail.com>
2022-02-22 17:59:20 +0500

    Array 23

---
+++ 11_array/array23.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, k, l int
+	fmt.Print("N = ")
+	fmt.Scan(&n)
+
+	ar := make([]float64, n, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&ar[i])
+	}
+
+	fmt.Print("K = ")
+	fmt.Scan(&k)
+	fmt.Print("L = ")
+	fmt.Scan(&l)
+
+	var sum float64
+
+	for i := 0; i < k-1; i++ {
+		sum += ar[i]
+	}
+
+	for i := l; i < n; i++ {
+		sum += ar[i]
+	}
+
+	fmt.Printf("\n%.2f\n", sum/float64(n-l+k-1))
+}
