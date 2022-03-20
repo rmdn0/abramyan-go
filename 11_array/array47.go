ea209b32f29f2fe04ef6ade78fe005f92d38ed05
Siyovush <paraparadox@protonmail.com>
2022-03-20 20:43:39 +0500

    Refactor Array 47

--- 11_array/array47.go
+++ 11_array/array47.go
@@ -8,22 +8,24 @@ func main() {
 	fmt.Scan(&n)
 
 	ar := make([]int, n, n)
-	var distinctElements []int
 	for i := 0; i < n; i++ {
 		fmt.Scan(&ar[i])
-		if !isNumberPresentInSlice(distinctElements, ar[i]) {
-			distinctElements = append(distinctElements, ar[i])
+	}
+
+	amount := 0
+	var check bool
+	for i := 0; i < n; i++ {
+		check = true
+		for j := 0; j < i; j++ {
+			if ar[i] == ar[j] {
+				check = false
+				break
+			}
+		}
+		if check {
+			amount++
 		}
 	}
 
-	fmt.Println(len(distinctElements))
-}
-
-func isNumberPresentInSlice(ar []int, x int) bool {
-	for i := 0; i < len(ar); i++ {
-		if ar[i] == x {
-			return true
-		}
-	}
-	return false
+	fmt.Println(amount)
 }
