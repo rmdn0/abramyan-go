ed5b3802557d2e76216162988b38f8b56ed35dfd
Siyovush <paraparadox@protonmail.com>
2022-03-21 09:10:56 +0500

    Array 51-60

---
+++ 11_array/array54.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("N = ")
+	fmt.Scan(&n)
+
+	evenCounter := 0
+	a := make([]int, n, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
+		if isEven(a[i]) {
+			evenCounter++
+		}
+	}
+
+	b := make([]int, evenCounter, evenCounter)
+	var j int
+	for i := 0; i < n; i++ {
+		if isEven(a[i]) {
+			b[j] = a[i]
+			j++
+		}
+	}
+
+	fmt.Println("\nSize of array B:", evenCounter)
+	for i := 0; i < evenCounter; i++ {
+		fmt.Print(b[i], " ")
+	}
+	fmt.Println()
+}
+
+func isEven(x int) bool {
+	return x%2 == 0
+}
