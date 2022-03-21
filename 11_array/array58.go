40ac4b476aa176a433f2a8c838f2d418592a56e3
Siyovush <60045551+paraparadox@users.noreply.github.com>
2022-03-21 09:12:51 +0500

    Array 51-60

---
+++ 11_array/array58.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("N = ")
+	fmt.Scan(&n)
+
+	a := make([]float64, n, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
+	}
+
+	b := make([]float64, n, n)
+	var sum float64
+	for i := 0; i < n; i++ {
+		sum = 0
+		for j := 0; j <= i; j++ {
+			sum += a[j]
+		}
+		b[i] = sum
+	}
+
+	fmt.Println()
+	for i := 0; i < n; i++ {
+		fmt.Printf("%.2f ", b[i])
+	}
+	fmt.Println()
+}
