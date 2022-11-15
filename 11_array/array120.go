e277654c0489bbe91f9b2266ba9c41aba8470611
Siyovush <paraparadox@protonmail.com>
2022-11-15 11:35:25 +0500

    Array 111-120 (#10)
    * Array 111
    
    * Array 112: Bubble sort
    
    * Array 113: Selection sort
    
    * Array 114: Insertion sort
    
    * Array 115
    
    * Array 116: Compression
    
    * Array 117
    
    * Array 118
    
    * Array 119
    
    * Array 120

---
+++ 11_array/array120.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("N = ")
+	fmt.Scan(&n)
+
+	ar := make([]int, n, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&ar[i])
+	}
+
+	tempAr := []int{}
+	var count int
+	for i := 0; i < n; i++ {
+		count = 0
+		for i+count < n-1 && ar[i+count] == ar[i+count+1] {
+			count++
+		}
+		tempAr = append(tempAr, ar[i:i+count]...)
+		i += count
+	}
+	ar = tempAr
+	n = len(ar)
+	fmt.Println()
+	for i := 0; i < n; i++ {
+		fmt.Print(ar[i], " ")
+	}
+	fmt.Println()
+}
