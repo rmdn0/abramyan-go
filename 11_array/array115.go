fe8f95bf256831f53c3294397787ef961ca5942c
Siyovush <paraparadox@protonmail.com>
2022-11-15 10:31:19 +0500

    Array 115

---
+++ 11_array/array115.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("N = ")
+	fmt.Scan(&n)
+
+	ar := make([]float64, n, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&ar[i])
+	}
+
+	indexes := make([]int, n, n)
+	for i := 0; i < n; i++ {
+		indexes[i] = i + 1
+	}
+
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-1-i; j++ {
+			if ar[indexes[j]-1] > ar[indexes[j+1]-1] {
+				swap(&indexes[j], &indexes[j+1])
+			}
+		}
+	}
+
+	fmt.Println()
+	for i := 0; i < n; i++ {
+		fmt.Print(indexes[i], " ")
+	}
+	fmt.Println()
+}
+
+func swap(x, y *int) {
+	*x, *y = *y, *x
+}
